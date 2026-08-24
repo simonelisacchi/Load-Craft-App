405199867ee748d6de92588c3e21bda8d17fa1fb
simonelisacchi <simone.lisacchi2000@gmail.com>
2026-08-24 12:13:45 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Il base path va cambiato con il nome esatto della repo GitHub quando si pubblica
-// su GitHub Pages, es: base: '/nome-repo/'
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-})
