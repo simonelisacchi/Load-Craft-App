e43a27ff893714a6ca1114ff826fcf75a3ad1afc
simonelisacchi <simone.lisacchi2000@gmail.com>
2026-08-24 12:16:32 +0200

    Delete colorScale.js

--- colorScale.js
+++
@@ -1,7 +0,0 @@
-// Scala colore condivisa: dal blu (sforzo/valore basso) al rosso
-// (sforzo/valore alto). Usata sia dalla mappa del percorso sia dai
-// grafici, per coerenza visiva in tutta l'app.
-export function colorForIntensity(norm) {
-  const hue = 210 - 210 * Math.min(1, Math.max(0, norm))
-  return `hsl(${hue}, 75%, 50%)`
-}
